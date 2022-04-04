8ce103fbdf1d8d455670b650a58054b2c54eed02
anjitpariyar <anjitpariyar@gmail.com>
2022-04-05 01:42:23 +0545

    aut fix

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -68,20 +68,16 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  //
-  // return the headers to the context so httpLink can read them
-
   return {
     headers: {
       ...headers,
-
       authorization: "Bearer " + process.env.NEXT_PUBLIC_DATO_CMS_TOKEN,
     },
   };
 });
 
 export async function getStaticProps() {
+  console.log(process.env.NEXT_PUBLIC_DATO_CMS_TOKEN);
   const client = new ApolloClient({
     link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
