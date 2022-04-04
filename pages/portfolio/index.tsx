46e0dbfc297d75c922875537974ab89245d43cf2
anjitpariyar <anjitpariyar@gmail.com>
2022-04-05 01:40:03 +0545

    aunthentication failed gql

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -5,7 +5,14 @@ import { Container, Padding } from "styled/Common.styled";
 import { portData } from "utils/sample-data";
 import { Card } from "components/pages/portfolio";
 import { device } from "styled/Breakpoint";
-const IndexPage = () => (
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+const IndexPage = ({ datas }) => (
   <Layout title="NODAZI | Portfolio">
     <Section>
       <Container>
@@ -36,3 +43,58 @@ const Grid = styled.div`
     gap: 7px;
   }
 `;
+
+// graphql
+const query = gql`
+  query {
+    cource {
+      id
+      name
+      courseDetaild {
+        ... on CourseHeaderRecord {
+          smallTitle
+          bigTitle
+          buttonText
+          description
+        }
+      }
+    }
+  }
+`;
+
+// auth process
+const httpLink = createHttpLink({
+  uri: "https://graphql.datocms.com/",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  //
+  // return the headers to the context so httpLink can read them
+
+  return {
+    headers: {
+      ...headers,
+
+      authorization: "Bearer " + process.env.NEXT_PUBLIC_DATO_CMS_TOKEN,
+    },
+  };
+});
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+  const { data } = await client.query({
+    query: query,
+  });
+
+  console.log(data);
+
+  return {
+    props: {
+      datas: [],
+    },
+  };
+}
